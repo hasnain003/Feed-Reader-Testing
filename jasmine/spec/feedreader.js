30b06b8e39dbdc90dcf5c7002c9d16ec89dd71dc
hasnain003 <mdhasnain839@gmail.com>
2018-10-22 22:38:32 +0530

    Extra comment removed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test  loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -40,7 +40,7 @@ $(function() {
          });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -55,26 +55,17 @@ $(function() {
 
 
     /*
-    /* TODO: Write a new test suite named "The menu" */
+    /* Created a new test suite named "The menu" */
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This test ensures the menu element is
+         * hidden by default.
          */
-        /* beforeEach(function() {
-            const isMenuHidden = document.getElementsByClassName('menu-hidden');
-         });*/
-
-
          it('is hidden by default', function() {
             const isMenuHidden = document.getElementsByClassName('menu-hidden');
             expect(isMenuHidden.length).not.toBe(0);
          });
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* This test ensures the menu changes
+          * visibility when the menu icon is clicked.
           */
           it('changes visibility when the menu is clicked', function() {
             const isMenuHidden = document.getElementsByClassName('menu-hidden');
@@ -86,13 +77,11 @@ $(function() {
           });
       });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Created a new test suite named "Initial Entries" */
      describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This test ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
          beforeEach(function(done) {
             loadFeed(0,function() {
@@ -105,11 +94,10 @@ $(function() {
          });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Created a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* This test ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
         let firstFeed,secondFeed;
 
